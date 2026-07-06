c9ef32501ffde140d81647b58f2d751651a71d15
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-07-06 23:08:07 +0900

    20260706_퀵 정렬 알고리즘

--- 4. 퀵 정렬 알고리즘.cpp
+++ 4. 퀵 정렬 알고리즘.cpp
@@ -27,7 +27,7 @@ void quickSort(int *data, int start, int end) {
     int temp;
 
     while(i <= j) { // 엇갈릴 때 까지 반복
-        while(data[i] <= data[key]) { // 키 값보다 큰 값을 만날 때 까지 이동
+        while(data[i] <= data[key] && i <= end) { // 키 값보다 큰 값을 만날 때 까지 이동
             i++;
         }
         while(data[j] >= data[key] && j > start) { // 키 값보다 작은 값을 만날 때 까지 이동

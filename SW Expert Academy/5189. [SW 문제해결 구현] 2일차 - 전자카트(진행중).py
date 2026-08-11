516767f0f522eba47693fa6520d1586aa90fd9b1
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-08-11 12:39:38 +0900

    수정

--- SW Expert Academy/5189. [SW 문제해결 구현] 2일차 - 전자카트(진행중).py
+++ SW Expert Academy/5189. [SW 문제해결 구현] 2일차 - 전자카트(진행중).py
@@ -1,26 +1,24 @@
-# 중복 X 순열로 풀어보자!
+import itertools
+
 T = int(input())
 
-
-def Check(arr, aLen, n, lo, sum):
-    global result
-    if(result <= sum):
-        return
-    if((aLen - 1) <= n):
-        if result > sum:
-            print(f"{n}: {lo+1} -> {1}, sum: {sum}, result: {result}")
-            result = sum + arr[lo][0]
-        return
-    for i in range(n, aLen):
-        if i == n: continue
-        sum += arr[lo][i]
-        print(f"{n}: {lo+1} -> {i+1}, sum: {sum}, result: {result}")
-        Check(arr, aLen, n + 1, i, sum)
-
 for test_case in range(1, T + 1):
-    aLen = int(input())
-    arr = [list(map(int, input().split())) for _ in range(aLen)]
+    aLen = [i for i in range(1, int(input())+1)] # 순열 생성 [1, 2, 3]
+    aPers = list(itertools.permutations(aLen[1:]))
+    arr = [list(map(int, input().split())) for _ in range(len(aLen))]
     result = float('inf')
-    Check(arr, aLen, 0, 0, 0)
+    
+    for aPer in aPers:
+        sum = arr[0][1]
+        for num in aPer:
+            if num + 1 >= len(aPer):
+                sum += arr[num][0]
+            else:
+                sum += arr[num][num + 1]
+            if sum > result:
+                break
+        if result < sum:
+            result = sum
     
     print(f"#{test_case} {result}")
+

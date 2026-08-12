2010c30d858d1137855bc375b4f38c1cf26f06cb
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-08-12 23:49:26 +0900

    lambda 추가

--- SW Expert Academy/5202. [SW 문제해결 구현] 3일차 - 화물 도크(진행중).py
+++ SW Expert Academy/5202. [SW 문제해결 구현] 3일차 - 화물 도크(진행중).py
@@ -29,3 +29,32 @@ for test_case in range(1, T + 1):
         
     
     print(f"#{test_case} {timeCount}")
+
+
+
+# 이게 lambda 를 몰라서 개 어렵게 되어있는데. lambda 쓰면 개편해짐.
+"""
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    dock = []
+    
+    for _ in range(N):
+        start, end = map(int, input().split())
+        dock.append((start, end))
+    
+    # 1. 끝나는 시간을 기준으로 오름차순 정렬, 끝이 같다면 시작 시간 기준 오름차순 정렬
+    dock.sort(key=lambda x: (x[1], x[0]))
+    
+    timeCount = 0
+    end_time = 0  # 직전 작업이 끝난 시간
+    
+    for start, end in dock:
+        # 현재 작업의 시작 시간이 이전 작업의 종료 시간보다 크거나 같으면 선택
+        if start >= end_time:
+            end_time = end
+            timeCount += 1
+            
+    print(f"#{test_case} {timeCount}")
+"""

e2cd729d30d8c306eed805717b76bb062c3e168f
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-08-24 23:47:08 +0900

    수정 2

--- SW Expert Academy/1249. 보급로(실패, BFS로 다시 풀기).py
+++ SW Expert Academy/1249. 보급로(실패, BFS로 다시 풀기).py
@@ -1,21 +1,22 @@
 T = int(input())
 
-def Check(arr, i, j, sum, fix):
+def Check(arr, i, j, sum):
     global result
 
     if(sum >= result):
         return
     if(i == len(arr) - 1 and j == len(arr) - 1):
         result = sum
-        print(result)
         return
         
     direction = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
     for nDir in direction:
         if(i + nDir[0] < len(arr) - 1 and i + nDir[0] >= 0 and j + nDir[1] < len(arr) - 1 and j + nDir[1] >= 0):
-            if((i + nDir[0], j + nDir[1]) not in fix):
-                Check(arr, i + nDir[0], j + nDir[1], sum + arr[i + nDir[0]][j + nDir[1]], fix.append((i + nDir[0],j + nDir[1])))
+            if(arr[i + nDir[0]][j + nDir[1]] >= 0):
+                arr[i][j] = -1
+                print(sum, i, j)
+                Check(arr, i + nDir[0], j + nDir[1], sum + arr[i + nDir[0]][j + nDir[1]])
     
 
 for test_case in range(1, T + 1):
@@ -26,7 +27,5 @@ for test_case in range(1, T + 1):
             arr[i].append(int(j))
 
     result = float('inf')
-    Check(arr, 0, 0, 0, [])
-
-    result = 0
+    Check(arr, 0, 0, 0)
     print(f"#{test_case} {result}")

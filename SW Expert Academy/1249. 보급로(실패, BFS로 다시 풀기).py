e1d8aa2e80f632fa3e5146ce349c5671b26ecc87
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-08-26 23:38:03 +0900

    20260826__알고리즘 풀이

--- SW Expert Academy/1249. 보급로(실패, BFS로 다시 풀기).py
+++ SW Expert Academy/1249. 보급로(실패, BFS로 다시 풀기).py
@@ -1,23 +1,28 @@
+# 백트래킹(DFS) 를 구현하여 풀긴 하였으나 시관초과 뜬 코드
+# 이건 DFS 말고 다른 알고리즘으로 해야한다고...
+
 T = int(input())
 
-def Check(arr, i, j, sum):
+def Check(boolArray, i, j, sum):
     global result
 
     if(sum >= result):
         return
-    if(i == len(arr) - 1 and j == len(arr) - 1):
-        result = sum
+      
+    elif(i == N - 1 and j == N - 1):
+        if result >= sum:
+            result = sum
         return
-        
-    direction = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
-    for nDir in direction:
-        if(i + nDir[0] < len(arr) - 1 and i + nDir[0] >= 0 and j + nDir[1] < len(arr) - 1 and j + nDir[1] >= 0):
-            if(arr[i + nDir[0]][j + nDir[1]] >= 0):
-                arr[i][j] = -1
-                print(sum, i, j)
-                Check(arr, i + nDir[0], j + nDir[1], sum + arr[i + nDir[0]][j + nDir[1]])
+    else:
+        direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     
+        for nDir in direction:
+            if(i + nDir[0] < N and i + nDir[0] >= 0 and j + nDir[1] < N and j + nDir[1] >= 0):
+                if(not boolArray[i + nDir[0]][j + nDir[1]]):
+                    boolArray[i][j] = True
+                    Check(boolArray, i + nDir[0], j + nDir[1], sum + arr[i + nDir[0]][j + nDir[1]])
+                    boolArray[i][j] = False
 
 for test_case in range(1, T + 1):
     N = int(input())
@@ -27,5 +32,6 @@ for test_case in range(1, T + 1):
             arr[i].append(int(j))
 
     result = float('inf')
-    Check(arr, 0, 0, 0)
+    temp = [[False] * N for i in range(N)]
+    Check(temp, 0, 0, 0)
     print(f"#{test_case} {result}")
--- SW Expert Academy/5209. [SW 문제해결 구현] 5일차 - 최소 생산 비용.py
+++ SW Expert Academy/5209. [SW 문제해결 구현] 5일차 - 최소 생산 비용.py
@@ -1,34 +1,62 @@
+# 현재 노드 자체가 공장이라는 것을 상기시켜야함.
 T = int(input())
 
 def Check(c, n, k, sum): # c = 선택 노드, n = 현재 노드, k = 최종 노드, sum = 총 생산비용
     global best
     
-    if(best <= sum):
+    if(best <= sum): # 가지치기
         return
-    elif(n == k):
-        best = sum
+    elif(n == k): # 전부 사용
+        if best >= sum:
+            best = sum
+        return
     else:
-        # 여기에 함수 제작
-        for i in arr:
-            if c[n] == False: # 현재 노드가 미선택일시
-                c[n] = True
-                for j in i:
-                    sum += j
-                    Check(c, n + 1, k, sum)
-                    sum -= j # 백트레킹
-                c[n] = False # 백트레킹
+        for i in range(k):
+            if not c[i]:
+                c[i] = True
+                Check(c, n + 1, k, sum + arr[n][i])
+                c[i] = False
 
 for test_case in range(1 ,T + 1):
-
     N = int(input()) # 제품 수
     arr = [0 for i in range(N)]
     for i in range(N):
         arr[i] = list(map(int, input().split()))
 
     best = float('inf')
-    Check([False] * N, 0, N, 0)
+    Check([False] *N, 0, N, 0)
     
     print(f"#{test_case} {best}")
 
 
-    # 이거 choice 부분만 수정하면 될 듯 ㄹㅇ 최적의 것만 가져오고 있음.
+# 아래 코드는 현재 노드를 인자로 받고 있지만 안사용하여
+# 중첩 for 문이 만들어진 경우 (잘못 만듬, 시간초과 가능성 농후)
+
+T = int(input())
+
+def Check(c1, c2, n, k, sum): # c = 선택 노드, n = 현재 노드, k = 최종 노드, sum = 총 생산비용
+    global best
+    
+    if(best <= sum): # 가지치기
+        return
+    elif(n == k): # 전부 사용
+        best = sum
+    else:
+        for c_1 in range(len(c1)): # 공장 선택문
+            if c1[c_1] == False: # 현재 노드가 미선택일시
+                for c_2 in range(len(c1)): # 제품 선택문
+                    if c2[c_2] == False: # 현재 노드가 미선택일시
+                        c1[c_1], c2[c_2] = True, True # 공장 사용
+                        Check(c1, c2, n + 1, k, sum + arr[c_1][c_2])
+                        c1[c_1], c2[c_2] = False, False # 백트레킹
+
+for test_case in range(1 ,T + 1):
+    N = int(input()) # 제품 수
+    arr = [0 for i in range(N)]
+    for i in range(N):
+        arr[i] = list(map(int, input().split()))
+
+    best = float('inf')
+    Check([False] *N, [False] *N, 0, N, 0)
+    
+    print(f"#{test_case} {best}")

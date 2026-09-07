7c4a671e98d72994d7d9bbbccdd935d62fb00696
Kim Myeonggi <80801074+mongki123@users.noreply.github.com>
2026-09-07 22:48:21 +0900

    20260907__알고리즘공부

--- SW Expert Academy/5248. [SW 문제해결 구현] 6일차 - 그룹 나누기.py
+++ SW Expert Academy/5248. [SW 문제해결 구현] 6일차 - 그룹 나누기.py
@@ -1,31 +1,30 @@
+def FindParent(parent, x):
+    if parent[x] != x:
+        parent[x] = FindParent(parent, parent[x]) # 경로 압축(Path Compression)
+    return parent[x]
+
+def Union(parent, left, right):
+    parent_left = FindParent(parent, left)
+    parent_right = FindParent(parent, right)
+    if(parent_left != parent_right):
+        parent[parent_right] = parent_left
+
 T = int(input())
 
 for test_case in range(1, T + 1):
 
     N, M = map(int, input().split())
-    Narr = [[i] for i in range(1, N + 1)]
-    Mchoi = list(map(int, input().split()))
+    Marr = list(map(int, input().split()))
 
-    for i in range(M * 2):
-        if i % 2 == 0:
-            left = Mchoi[i]-1
-            right = Mchoi[i+1]-1
-            if left > right:
-                left, right = right, left
+    parent = [i for i in range(N + 1)]
 
-            if Narr[left][0] == left + 1:
-                Narr[left].append(right + 1)
-                Narr[right] = [left + 1]
-            else:
-                Narr[Narr[left][0]-1].append(right + 1)
-                Narr[right] = [Narr[left][0]]
-                
-    for i in range(N-1,0,-1):
-        if i >= len(Narr):
-            break
-        if(len(Narr[i]) == 1 and Narr[i][0] != i + 1):
-            Narr.pop(i)
-            
-    result = len(Narr)
-        
-    print(f"#{test_case} {result}")
+    for i in range(0, M * 2, 2):
+        leftV = Marr[i]
+        rightV = Marr[i + 1]
+        Union(parent, leftV, rightV)
+    
+    result = set()
+    for i in range(1, N + 1):
+        result.add(FindParent(parent,i))
+    
+    print(f"#{test_case} {len(result)}")
